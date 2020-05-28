6236bae47f4381a5665432e4e016da91b2a39f42
tkm <sumikawa.web@gmail.com>
2020-05-28 19:24:48 +0900

    Set output HTML,CSS,JavaScript

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './dev/assets/js/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: './assets/js/main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './assets/css/style.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './dev/index.html',
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin(),
+  ]
+};
